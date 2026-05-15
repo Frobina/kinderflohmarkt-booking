f8c9b7e9515386e0d4d751dd09769f079c797fd1
MMayer94 <M.Mayer2909@gmail.com>
2026-05-16 01:03:39 +0200

    git add app/main.py git commit -m „feat: Tag 9 - GET und POST Endpunkte für Tische in Main.py eingebunden git push

--- app/main.py
+++ app/main.py
@@ -1,12 +1,30 @@
 # app/main.py
-from fastapi import FastAPI
-from app.database import engine, Base
+from fastapi import FastAPI, Depends
+HTTPException
+from sqlalchemy.orm import Session
+from typing import List
 
-# Erstellt alle Tabellen in der SQLite-Datenbank, falls sie noch nicht existieren
+from app import crud, models, schemas
+from app.database import engine, Base, get_db
+
+# Tabellen initialisieren
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Kinderflohmarkt standbuchung API")
+app = FastAPI(title="Kinderflohmarkt Standbuchung API")
 
 @app.get("/")
 def read_root():
     return {"message": "Willkommen auf unserer Plattform des Kinderflohmarktes-Standbuchung-WIP!"}
+
+# Endpunkt: Alle Tische anzeigen
+@app.get("/tables/",
+         response_model=List[schemas.TableResponse])
+def read_tables(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    tables = crud.get_tables(db, skip=skip, limit=limit)
+    return tables
+
+#Endpunkt: Einen neuen Tisch anlegen
+@app.post("/tables/",
+response_model=schemas.TableResponse)
+def create_table(table: schemas.TableCreate, db: Session = Depends(get_db)):
+    return crud.create_table(db=table=table)
