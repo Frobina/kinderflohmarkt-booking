ac6293833d6eaaa1d9533d6160eef46071a32d8f
MMayer94 <marianne.mayer@student.syntax-institut.de>
2026-05-16 01:50:52 +0200

    update

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 # app/main.py
-from fastapi import FastAPI, Depends
-HTTPException
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 
@@ -17,14 +16,12 @@ def read_root():
     return {"message": "Willkommen auf unserer Plattform des Kinderflohmarktes-Standbuchung-WIP!"}
 
 # Endpunkt: Alle Tische anzeigen
-@app.get("/tables/",
-         response_model=List[schemas.TableResponse])
+@app.get("/tables/", response_model=List[schemas.TableResponse])
 def read_tables(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     tables = crud.get_tables(db, skip=skip, limit=limit)
     return tables
 
-#Endpunkt: Einen neuen Tisch anlegen
-@app.post("/tables/",
-response_model=schemas.TableResponse)
+# Endpunkt: Einen neuen Tisch anlegen
+@app.post("/tables/", response_model=schemas.TableResponse)
 def create_table(table: schemas.TableCreate, db: Session = Depends(get_db)):
     return crud.create_table(db=table=table)
